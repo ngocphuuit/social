f2dd21fcf02a367cbe0dd625b44d0c5d4b85b753
Administrator <admin@example.com>
2015-05-22 21:08:12 +0700

    product detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,12 @@
 class ProductsController < ApplicationController
+	before_action :check_login, only: [:create, :newreceipt]
 
 	def index
 	end
 
 	def detail
+		@product = Product.find(params[:product_id])
+		puts(@product)
 	end
 
 	def category
@@ -21,8 +24,9 @@ class ProductsController < ApplicationController
 	def create
 		@product = Product.new(product_params)
 		@product.photo = params[:product][:photo]
+		@product.user_id = session[:user]["id"]
 		if @product.save
-			redirect_to products_detail_path
+			redirect_to products_detail_path(product_id: @product.id)
 		else
 			redirect_to products_newreceipts_path
 		end
@@ -31,5 +35,11 @@ class ProductsController < ApplicationController
 	private
 		def product_params
 			params.require(:product).permit(:title, :description, :status, :quantity)
+		end
+
+		def check_login
+			if !session[:user]
+				redirect_to users_login_path
+			end
 		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,22 +7,22 @@ Rails.application.routes.draw do
 
   ### USERS
 
-  get   'users/info'          => 'users#info'
-  get   'users/login'         => 'users#login_form'
-  post  'users/login'         => 'users#login'
-  get   'users/register'      => 'users#register_form'
-  post  'users/register'      => 'users#register'
+  get   'users/info'                        => 'users#info'
+  get   'users/login'                       => 'users#login_form'
+  post  'users/login'                       => 'users#login'
+  get   'users/register'                    => 'users#register_form'
+  post  'users/register'                    => 'users#register'
 
   ### Product
-  get   'products'            => 'products#receipts'
-  get   'products/detail'     => 'products#detail'
-  get   'products/newreceipts'=> 'products#newreceipt'
-  get   'products/search'     => 'products#search'
-  post  'products/create'     => 'products#create'
+  get   'products'                          => 'products#receipts'
+  get   'products/detail/:product_id'       => 'products#detail' , as: 'products_detail'
+  get   'products/newreceipts'              => 'products#newreceipt'
+  get   'products/search'                   => 'products#search'
+  post  'products/create'                   => 'products#create'
 
   ### Welcome
-  get   'about'               => 'welcome#about'
-  get   'contact'             => 'welcome#contact'
-  get   'blog'                => 'welcome#blog'
-  get   'blogdetail'          => 'welcome#blogdetail'
+  get   'about'                             => 'welcome#about'
+  get   'contact'                           => 'welcome#contact'
+  get   'blog'                              => 'welcome#blog'
+  get   'blogdetail'                        => 'welcome#blogdetail'
 end
